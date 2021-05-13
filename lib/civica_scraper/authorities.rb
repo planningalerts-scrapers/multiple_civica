a260177cbafa8e63651119ca0187ec14c7f4e110
Matthew Landauer <mlandauer@gmail.com>
2021-05-13 10:53:24 +1000

    Remove councils in SA because they are now all using the SA Planning Portal

--- lib/civica_scraper/authorities.rb
+++ lib/civica_scraper/authorities.rb
@@ -44,35 +44,9 @@ module CivicaScraper
       url: "https://eservices.cairns.qld.gov.au/eservice/daEnquiryInit.do?nodeNum=227",
       period: :last30days
     },
-    mount_gambier: {
-      url: "https://ecouncil.mountgambier.sa.gov.au/eservice/daEnquiryInit.do?nodeNum=21461",
-      period: :last2months
-    },
-    norwood: {
-      url: "https://ecouncil.npsp.sa.gov.au/eservice/daEnquiryInit.do?doc_typ=155&nodeNum=10209",
-      period: :lastmonth
-    },
-    tea_tree_gully: {
-      url: "https://www.ecouncil.teatreegully.sa.gov.au/eservice/daEnquiryInit.do?nodeNum=131612",
-      period: :lastmonth,
-      # One of the intermediate certificates had expired. So rather than waiting
-      # for them to fix this just...
-      disable_ssl_certificate_check: true
-    },
-    loxton_waikerie: {
-      url: "https://eservices.loxtonwaikerie.sa.gov.au/eservice/daEnquiryInit.do?nodeNum=2811",
-      period: :lastmonth
-    },
     orange: {
       url: "https://ecouncil.orange.nsw.gov.au/eservice/daEnquiryInit.do?nodeNum=24",
       period: :last30days
-    },
-    gawler: {
-      url: "https://eservices.gawler.sa.gov.au/eservice/daEnquiryInit.do?doc_typ=4&nodeNum=3228",
-      period: :lastmonth,
-      # Has an incomplete SSL chain: See
-      # https://www.ssllabs.com/ssltest/analyze.html?d=eservices.gawler.sa.gov.au
-      disable_ssl_certificate_check: true
     },
     woollahra: {
       url: "https://eservices.woollahra.nsw.gov.au/eservice/daEnquiryInit.do?nodeNum=5270",
